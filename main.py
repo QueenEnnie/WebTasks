2b62ae9a7818a10cb3c76464f2484135fadaa084
Ksenia <kcy.2006@mail.ru>
2023-03-22 18:41:34 +0300

    flask-wtf auto-answer

--- main.py
+++ main.py
@@ -20,5 +20,19 @@ def job_list(list):
     return render_template("job_list.html", list_type=list)
 
 
+@app.route('/answer')
+@app.route('/auto_answer')
+def form_answer():
+    information = {"title": "Анкета",
+                   "surname": "Watny",
+                   "name": "Mark",
+                   "education": "выше среднего",
+                   "profession": "штурман марсохода",
+                   "sex": "male",
+                   "motivation": "Всегда мечтал застрять на Марсе!",
+                   "ready": "True"}
+    return render_template("auto_answer.html", dictionary=information, title=information["title"])
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
